61442f06aa730db953b1aee25d19eec3b8c65943
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-02 23:45:32 +0500

    removed some comments and confirmed that app is working fine

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<p>GAME OF LIFE</p>, document.getElementById('root'));
