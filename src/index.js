5a38ccb280845cdac6a93dd3a8f244e7f0d74493
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-03 21:21:46 +0500

    Barebone structure

--- src/index.js
+++ src/index.js
@@ -2,4 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<p>GAME OF LIFE</p>, document.getElementById('root'));
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>
+        Grid
+      </div>
+    )
+  }
+}
+
+
+class Main extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      generation: 0
+    };
+  }
+  
+  render() {
+    return(
+      <div>
+        <h1>Game of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
