3eb60e42d1ccb7945ba7797a5acb1423447ef7c2
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-06-03 22:19:14 +0500

    add rules and buttons component

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 
 
 class Box extends React.Component {
-  
+
   selectBox = () => {
     this.props.selectBox(this.props.row, this.props.col);
   }
-  
-  render() {    
+
+  render() {
     return (
       <div
-        className = {this.props.boxClass}
+        className={this.props.boxClass}
         id={this.props.id}
         onClick={this.selectBox}
       />
@@ -47,9 +47,17 @@ class Grid extends React.Component {
 
     return (
       <div className="grid" style={{ width: width }}>
-        { rowsArr }
+        {rowsArr}
       </div>
     )
+  }
+}
+
+class Buttons extends React.Component {
+  render() {
+    return (
+      
+    );
   }
 }
 
@@ -68,7 +76,7 @@ class Main extends React.Component {
       gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     };
   }
-  
+
   selectBox = (row, col) => {
     let gridCopy = arrayClone(this.state.gridFull);
     gridCopy[row][col] = !gridCopy[row][col];
@@ -76,29 +84,74 @@ class Main extends React.Component {
       gridFull: gridCopy
     })
   }
-  
+
   seed = () => {
     let gridCopy = arrayClone(this.state.gridFull);
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        if(Math.floor(Math.random() * 4) === 1) {
+        if (Math.floor(Math.random() * 4) === 1) {
           gridCopy[i][j] = true;
         }
-      }      
+      }
     }
     this.setState({
       gridFull: gridCopy
     })
   }
-  
+
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  }
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0;
+        if (i > 0 && g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+        if (!g[i][j] && count === 3) g2[i][j] = true;
+      }
+    }
+
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1
+    })
+  }
+
   componentDidMount() {
     this.seed();
+    this.playButton();
   }
 
   render() {
     return (
       <div>
         <h1>Game of Life</h1>
+        <Buttons
+          playButton={this.playButton}
+          pause={this.pauseButton}
+          slow={this.slow}
+          fast={this.fast}
+          clear={this.clear}
+          seed={this.seed}
+          gridSize={this.gridSize}
+        />
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
